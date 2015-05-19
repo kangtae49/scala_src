6ab5e2c9f958d235f07ad261c2ecd41dbfa9f99d
kangtae49 <kangtae49@gmail.com>
2015-05-19 23:47:08 +0900

    add remote

--- akka_ex1/src/main/scala/hello.scala
+++ akka_ex1/src/main/scala/hello.scala
@@ -1,27 +1,42 @@
-import akka.actor.{Props, Actor, ActorSystem}
-
 /**
  * Created by ktkim on 2015-05-19.
  */
+import akka.actor.SupervisorStrategy.{Escalate, Restart, Resume}
+import akka.actor._
+import scala.concurrent.duration._
 
 
-case class EfMsg(msg: String)
+case class EfMsg(msg: String) extends Serializable
 
-class TestActor extends Actor {
+class TestActor extends Actor with ActorLogging {
 
+  override val supervisorStrategy = {
+    OneForOneStrategy(maxNrOfRetries = 10, withinTimeRange = 1 minute){
+      case _: ArithmeticException      ⇒ Resume
+      case _: NullPointerException     ⇒ Restart
+      case _: Exception                ⇒ Escalate
+    }
+  }
+
+
+  val testActor2 = context.actorOf(Props[TestActor2], "testactor2")
 
   def run_job(msg: String) = {
     println(s"[TestActor] msg: $msg")
-    val testActor2 = context.actorOf(Props[TestActor2], "testactor2")
-    testActor2 ! EfMsg(msg)
+
+    testActor2 forward EfMsg("[child1]" ++ msg)
+    testActor2 forward EfMsg("[child2]" ++ msg)
+
   }
 
-  def receive = {
+  override def receive = {
     case EfMsg(msg) => run_job(msg)
   }
+
+
 }
 
-class TestActor2 extends Actor {
+class TestActor2 extends Actor with ActorLogging {
 
 
   def run_job(msg: String) = {
@@ -35,14 +50,38 @@ class TestActor2 extends Actor {
 
 
 object hello {
+  val system = ActorSystem("hello")
+
+  def test() = {
+    val tt = system.actorSelection("akka.tcp://hello@192.168.56.1:2552/user/testActor")
+    println("tt:" + tt)
+    tt ! EfMsg("kkkkkkkkkkkkk")
+  }
 
   def main(args: Array[String]) {
     println(args mkString " ")
 
-    val system = ActorSystem("hello")
 
-    val testActor = system.actorOf(Props[TestActor], "testactor")
-    testActor ! EfMsg("kkt")
+
+    val testActor = system.actorOf(Props[TestActor], "testActor")
+
+    testActor ! EfMsg("kkt-1")
+    println("testActor:" + testActor)
+
+    Thread sleep 5000
+
+    test ()
+
+
+    /*
+    testActor ! EfMsg("kkt-1")
+    Thread sleep 1000
+    testActor ! EfMsg("kkt-2")
+    Thread sleep 1000
+    testActor ! EfMsg("kkt-3")
+    */
+
+    //Thread sleep 5000
 
     println("terminated")
 
